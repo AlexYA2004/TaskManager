ed44bc5816902142338f189f297392d234ba0846
ayakimenko <aakimeno49@gmail.com>
2025-07-11 13:16:49 +0300

    Добавлена конфигурация Elastic

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,7 +1,34 @@
+using Nest;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information() 
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", "TaskManager.API")
+    .WriteTo.Console()
+    .WriteTo.File("C:\\Users\\aakim\\OneDrive\\Desktop\\logs.txt.txt")
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        IndexFormat = "api-logs",
+        AutoRegisterTemplate = true,
+        NumberOfShards = 1, 
+        NumberOfReplicas = 0,
+        ModifyConnectionSettings = x => x
+            .ServerCertificateValidationCallback((o, cert, chain, errors) => true)
+            .DisableAutomaticProxyDetection(),
+        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
+    })
+    .CreateLogger();
+
+
+builder.Host.UseSerilog();
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
